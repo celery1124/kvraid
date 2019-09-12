0b82ebdbe363b2ff27a46a04158835f85fd288f9
Mian Qin <celery1124@tamu.edu>
2019-09-12 16:05:59 -0500

    minor change

--- kvraid/src/kvraid.cpp
+++ kvraid/src/kvraid.cpp
@@ -7,7 +7,7 @@
 
 #define RECLAIMS_BULKS 2 * k_
 #define MAX_TRIM_NUM 1024
-#define TRIM_GUARD_NUM 512
+#define TRIM_GUARD_NUM 1024
 
 #define GC_DEV_USAGE_VOL_RATIO_THRES 2
 #define GC_DEV_UTIL_THRES 0.5
