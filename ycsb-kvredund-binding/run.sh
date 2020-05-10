4ba67dcde0ce79564cb312f0dd6a4006a72507f1
Mian Qin <celery1124@tamu.edu>
2020-05-09 20:18:20 -0700

    unrecognized commit

--- ycsb-kvredund-binding/run.sh
+++ ycsb-kvredund-binding/run.sh
@@ -12,12 +12,14 @@ threads="64"
 #tests="evalf"
 #tests="evala_constant evalb_constant evalc_constant evald_constant evale_constant evalf_constant evala_uniform evalb_uniform evalc_uniform evald_uniform evale_uniform evalf_uniform"
 #tests="evala_uniform evalb_uniform evalc_uniform evald_uniform evalf_uniform wr91_uniform wr82_uniform wr73_uniform wr64_uniform"
-tests="wr91_uniform wr73_uniform wr55_uniform wr37_uniform wr19_uniform"
-kvredund_type="1 2 0 3" # 0-KVRaid 1-KVEC 2-KVMirror 3-KVRaidPack
-meta_type="1 2" # 0-Mem 1-Storage (leveldb) 2-CockoHashMap
-gc_ena="1"
+tests="wr73_uniform wr37_uniform"
+#tests="wr91_uniform wr73_uniform wr55_uniform wr37_uniform wr19_uniform"
+#tests="recovery_uniform"
+kvredund_type="0 " # 0-KVRaid 1-KVEC 2-KVMirror 3-KVRaidPack
+meta_type=" 2 " # 0-Mem 1-Storage (leveldb) 2-CockoHashMap
+gc_ena="0"
 
-dev_cap='85899345920'
+dev_cap='100'
 sed -i 's/\"dev_cap\":.*/\"dev_cap\":'${dev_cap}',/' kvredund_config.json
 recordcnt='50000000'
 sed -i 's/recordcount=.*/recordcount='$recordcnt'/' workloads/*_uniform
@@ -43,6 +45,8 @@ do
 				for testfile in $tests
 				do
 					result_txt=$result_dir/${testfile}_${kv_type}_${m_type}_${gc_en}_${exp_id}
+					result_save_dir=$result_dir/${testfile}
+					mkdir -p $result_save_dir
 					# clean file if existed
 					echo "" > $result_txt
 					for numofthreads in $threads
@@ -60,17 +64,25 @@ do
 						
 						sleep 60
 						# ycsb load
-						./bin/ycsb load kvredund -s -P workloads/$testfile -threads $numofthreads -p maxexecutiontime=1800 -jvm-args="-Xms16g -Xmx16g" 2> err.log 
-							
+						/usr/bin/time -v ./bin/ycsb load kvredund -s -P workloads/$testfile -threads $numofthreads -p maxexecutiontime=900 -jvm-args="-Xms16g -Xmx16g" 1>out.log 2> err.log 
+						cp out.log $result_save_dir/load_out_${kv_type}_${m_type}_${gc_en}_${exp_id}.log
+						cp err.log $result_save_dir/load_err_${kv_type}_${m_type}_${gc_en}_${exp_id}.log
+						cp kv_device.log $result_save_dir/load_dev_${kv_type}_${m_type}_${gc_en}_${exp_id}.log
+
 						echo load >> $result_txt
 						printf "load_tp: " >> $result_txt
 						sed '/CLEANUP/d' err.log |grep "operations" |awk '{print $7}'|awk '{if(NR>5)SUM+=$1} END{print SUM/(NR-3)}' >> $result_txt
 						printf "\n" >> $result_txt
 						printf "load_lat: " >> $result_txt
-						sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/INSERT/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
+						grep "INSERT" out.log | grep "AverageLatency" | awk '{print $3}' >> $result_txt
+						#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/INSERT/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
+						printf "\n" >> $result_txt
+                                                printf "load_lat_95th: " >> $result_txt
+						grep "INSERT" out.log | grep "95thPercentileLatency" | awk '{print $3}' >> $result_txt 
 						printf "\n" >> $result_txt
 						printf "load_lat_99th: " >> $result_txt
-						sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/INSERT/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
+						grep "INSERT" out.log | grep "99thPercentileLatency" | awk '{print $3}' >> $result_txt
+						#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/INSERT/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
 						printf "\n" >> $result_txt
 						# report io
 						printf "store_ios: " >> $result_txt
@@ -84,16 +96,26 @@ do
 						# report device usage
 						printf "dev_usage: " >> $result_txt
 						cat kv_device.log|grep "usage"| awk '{ SUM += $2} END { print SUM }' >> $result_txt
+						# report cpu
+						cpu_user=`cat err.log | grep "User time" | awk '{print $4}'`
+						cpu_sys=`cat err.log | grep "System time" | awk '{print $4}'`
+						printf "cpu_time: " >> $result_txt
+						echo "($cpu_user+$cpu_sys)"|bc >> $result_txt
+						printf "cpu_util: " >> $result_txt
+						cat err.log | grep "CPU" | awk '{print $7}' >> $result_txt
 
-						rm -rf kv_device.log err.log
+						rm -rf kv_device.log err.log out.log
 						sleep 10
 
 						# ycsb run 
 						retry_cnt=0
 						while :
 						do
-							./bin/ycsb run kvredund -s -P workloads/$testfile -threads $numofthreads -p maxexecutiontime=1800 -jvm-args="-Xms16g -Xmx16g"  2> err.log  
-							
+							/usr/bin/time -v ./bin/ycsb run kvredund -s -P workloads/$testfile -threads $numofthreads -p maxexecutiontime=900 -jvm-args="-Xms16g -Xmx16g"  1>out.log 2> err.log  
+							cp out.log $result_save_dir/run_out_${kv_type}_${m_type}_${gc_en}_${exp_id}.log
+							cp err.log $result_save_dir/run_err_${kv_type}_${m_type}_${gc_en}_${exp_id}.log
+							cp kv_device.log $result_save_dir/run_dev_${kv_type}_${m_type}_${gc_en}_${exp_id}.log
+
 							if test -n "$(find ./ -maxdepth 1 -name 'hs_err*' -print -quit)"
 							then
 								rm hs_err*
@@ -118,34 +140,59 @@ do
 							sed '/CLEANUP/d' err.log |grep "operations" |awk '{print $7}'|awk '{if(NR>5)SUM+=$1} END{print SUM/(NR-3)}' >> $result_txt
 							printf "\n" >> $result_txt
 							printf "insert_lat: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/INSERT/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
+							grep "INSERT" out.log | grep "AverageLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/INSERT/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
 							printf "\n" >> $result_txt
-							printf "load_lat_99th: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/INSERT/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
+							printf "insert_lat_95th: " >> $result_txt
+							grep "INSERT" out.log | grep "95thPercentileLatency" | awk '{print $3}' >> $result_txt
+							printf "\n" >> $result_txt
+                                                        printf "insert_lat_99th: " >> $result_txt
+                                                        grep "INSERT" out.log | grep "99thPercentileLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/INSERT/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
 							printf "\n" >> $result_txt
 							printf "update_lat: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/UPDATE/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
+							grep "UPDATE" out.log | grep "AverageLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/UPDATE/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
 							printf "\n" >> $result_txt
-							printf "update_lat_99th: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/UPDATE/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
+							printf "update_lat_95th: " >> $result_txt
+							grep "UPDATE" out.log | grep "95thPercentileLatency" | awk '{print $3}' >> $result_txt
+                                                        printf "\n" >> $result_txt
+                                                        printf "update_lat_99th: " >> $result_txt
+                                                        grep "UPDATE" out.log | grep "99thPercentileLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/UPDATE/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
 							printf "\n" >> $result_txt
 							printf "read_lat: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/READ/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
+							grep "READ" out.log | grep "AverageLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/READ/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
 							printf "\n" >> $result_txt
-							printf "read_lat_99th: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/READ/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
+							printf "read_lat_95th: " >> $result_txt
+							grep "READ" out.log | grep "95thPercentileLatency" | awk '{print $3}' >> $result_txt
+                                                        printf "\n" >> $result_txt
+                                                        printf "read_lat_99th: " >> $result_txt
+                                                        grep "READ" out.log | grep "99thPercentileLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/READ/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
 							printf "\n" >> $result_txt
 							printf "scan_lat: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/SCAN/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
+							grep "SCAN" out.log | grep "AverageLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/SCAN/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
 							printf "\n" >> $result_txt
-							printf "scan_lat_99th: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/SCAN/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
+							printf "scan_lat_95th: " >> $result_txt
+							grep "SCAN" out.log | grep "95thPercentileLatency" | awk '{print $3}' >> $result_txt
+                                                        printf "\n" >> $result_txt
+                                                        printf "scan_lat_99th: " >> $result_txt
+                                                        grep "SCAN" out.log | grep "99thPercentileLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/SCAN/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
 							printf "\n" >> $result_txt
 							printf "rmw_lat: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/READMODIFYWRITE/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
+							grep "READMODIFYWRITE" out.log | grep "AverageLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/READMODIFYWRITE/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| awk '{if(NR>5)SUM+=$9} END{print SUM/(NR-3)}' >> $result_txt
 							printf "\n" >> $result_txt
-							printf "rmw_lat_99th: " >> $result_txt
-							sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/READMODIFYWRITE/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
+							printf "rmw_lat_95th: " >> $result_txt
+							grep "READMODIFYWRITE" out.log | grep "95thPercentileLatency" | awk '{print $3}' >> $result_txt
+                                                        printf "\n" >> $result_txt
+                                                        printf "rmw_lat_99th: " >> $result_txt
+                                                        grep "READMODIFYWRITE" out.log | grep "99thPercentileLatency" | awk '{print $3}' >> $result_txt
+							#sed '/CLEANUP/d' err.log |grep "operations" |awk '{flag=0; j=0;for(i=1;i<NF;i++){if ($i~/READMODIFYWRITE/) {flag=1}; if(flag==1) {printf("%s ",$i); if(j++==8) break;}} printf("\n")}'|sed 's/=/ /g'| sed 's/,/ /g' | awk 'BEGIN {max=0} {if($13>max) max=$13} END {print max}' >> $result_txt
 							printf "\n" >> $result_txt
 
 							# report io
@@ -176,6 +223,14 @@ do
 							printf "invalid-alive-group3: " >> $result_txt
 							cat kv_device.log|grep "invalid-alive" | awk '{ SUM += $10} END { print SUM }' >> $result_txt
 							printf "\n" >> $result_txt
+
+							# report cpu
+							cpu_user=`cat err.log | grep "User time" | awk '{print $4}'`
+							cpu_sys=`cat err.log | grep "System time" | awk '{print $4}'`
+							printf "cpu_time: " >> $result_txt
+							echo "($cpu_user+$cpu_sys)"|bc >> $result_txt
+							printf "cpu_util: " >> $result_txt
+							cat err.log | grep "CPU" | awk '{print $7}' >> $result_txt
 
 							echo "" >> $result_txt
 							rm -rf *.log
