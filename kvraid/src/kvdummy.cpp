2ddb7290683286983c77ff2a887568bd13ccbc4a
Mian Qin <celery1124@tamu.edu>
2020-05-23 23:53:38 -0500

    add cache to kvdummy (for motivation data collection)

--- kvraid/src/kvdummy.cpp
+++ kvraid/src/kvdummy.cpp
@@ -26,6 +26,9 @@ bool KVDummy::kvr_update(kvr_key *key, kvr_value *value) {
     int dev_idx = get_dev_idx(key->key, key->length);
 
     std::string skey(key->key, key->length);
+    // Evict from cache
+    kvr_erase_cache(skey);
+    
     std::string sval(value->val, value->length);
     
     ssds_[dev_idx].kv_store(&skey, &sval);
@@ -44,11 +47,23 @@ bool KVDummy::kvr_get(kvr_key *key, kvr_value *value) {
     int dev_idx = get_dev_idx(key->key, key->length);
 
     std::string skey(key->key, key->length);
+    // First, search for cache
+    Cache::Handle *h = kvr_read_cache(skey, value);
+
+    if (h != NULL) { // hit in cache
+        kvr_release_cache(h);
+        return true;
+    }
+
     value->val = (char*)malloc(MAX_VAL_SIZE);
     phy_val pval(value->val, MAX_VAL_SIZE);
 
     ssds_[dev_idx].kv_get(&skey, &pval);
     value->length = pval.actual_len;
+
+    // insert to cache
+    h = kvr_insert_cache(skey, value);
+    kvr_release_cache(h);
 
     return true;
 }
